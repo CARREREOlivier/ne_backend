46f788a1df954fb384fdaab297277a3a5e906ef6
CARREREOlivier <carrereolivier@yahoo.fr>
2024-10-29 15:02:33 +0100

    Fix : path to dependencies.

--- controllers/LetsPlayViewController.php
+++ controllers/LetsPlayViewController.php
@@ -1,7 +1,7 @@
 <?php
-require_once __DIR__ . '../repositories/LetsPlayRepository.php';
-require_once __DIR__ . '../models/LetsPlayModel.php';
-require_once __DIR__ . '../config/db.php';
+require_once __DIR__ . '/../repositories/LetsPlayRepository.php';
+require_once __DIR__ . '/../models/LetsPlayModel.php';
+require_once __DIR__ . '/../config/db.php';
 
 class LetsPlayViewController {
     private $letsPlayRepository;
