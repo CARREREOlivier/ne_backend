f3adc14083ed00a4374245669adcacaa0b6062e8
CARREREOlivier <carrereolivier@yahoo.fr>
2024-10-29 15:01:52 +0100

    Fix : connection logic wasn't working properly

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,18 +1,27 @@
 <?php
 require_once __DIR__ . '/../repositories/UserRepository.php';
-require_once __DIR__ . '/../dao/UserDAO.php';
-
+require_once __DIR__ . '/../config/db.php';
 class UserController {
     private UserRepository $userRepository;
 
     public function __construct() {
-        // Passer l'instance de UserDAO au constructeur de UserRepository
-        $this->userRepository = new UserRepository(new UserDAO());
+        $db = new Database();  // Connexion à la base de données
+        $this->userRepository = new UserRepository($db->getConnection());
     }
 
     public function getAllUsers(): void {
         $users = $this->userRepository->findAll();
         echo json_encode($users);
+    }
+
+    public function getUserById($id) {
+        $user = $this->userRepository->findById($id);
+        if ($user) {
+            echo json_encode($user);
+        } else {
+            http_response_code(404);
+            echo json_encode(['message' => 'Utilisateur non trouvé']);
+        }
     }
 }
 ?>
